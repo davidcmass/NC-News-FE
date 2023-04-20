9829712d8a6d81941a8b8fcfd1d70e5682b959f2
DavidMass <112913387+davidcmass@users.noreply.github.com>
2023-04-21 00:48:33 +0100

    Update Nav.jsx

--- src/components/ArticleCards.jsx
+++ src/components/ArticleCards.jsx
@@ -19,7 +19,7 @@ const ArticleCards = ({ setIsLoading }) => {
       const aBody = article.body.substring(0, 120) + '...'
 
       if (article.article_id < 10) {
-      return <div className='articleCard flex flex-col p-5 text-left gap-1' key={article.article_id}>
+      return <div className={'articleCard flex flex-col p-5 text-left gap-1'} key={article.article_id}>
       <img className='pb-2' src={article.article_img_url} alt="image" />
         <p className='topic'>{article.topic}</p>
       <a href={`Articles/${article.article_id}`}><h2 className="articleTitle">{article.title}</h2></a>
